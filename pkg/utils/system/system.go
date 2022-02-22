b5ecf41fbc705b35fae60bbf37d06676fa724b63
likun.zhang <likun.zhang@cloudminds.com>
2022-02-22 13:02:14 +0800

    perf(config): perf config

--- pkg/utils/system/system.go
+++ pkg/utils/system/system.go
@@ -6,41 +6,34 @@ import (
 	"io/ioutil"
 )
 
-// nolint: lll
 type Options struct {
 	Listen   string `json:"listen,omitempty" description:"listen address"`
 	CAFile   string `json:"caFile,omitempty" description:"ca file path"`
 	CertFile string `json:"certFile,omitempty" description:"cert file path"`
 	KeyFile  string `json:"keyFile,omitempty" description:"key file path"`
-	CertDir  string `json:"certDir,omitempty" description:"cert dir path,if not empty will use certDir and ca.crt tls.crt tls.key instead of caFile certFile and keyFile"`
 }
 
 func NewDefaultOptions() *Options {
 	return &Options{
 		Listen:   ":8080",
-		CertDir:  "certs",
-		CAFile:   "certs/ca.crt",
-		CertFile: "certs/tls.crt",
-		KeyFile:  "certs/tls.key",
+		CAFile:   "",
+		CertFile: "",
+		KeyFile:  "",
 	}
 }
 
 func (o *Options) IsTLSConfigEnabled() bool {
-	return (o.CertFile != "" && o.KeyFile != "") || o.CertDir != ""
+	return (o.CertFile != "" && o.KeyFile != "")
 }
 
 func (o *Options) ToTLSConfig() (*tls.Config, error) {
 	cafile, certfile, keyfile := o.CAFile, o.CertFile, o.KeyFile
-	if o.CertDir != "" {
-		cafile = o.CertDir + "/ca.crt"
-		certfile = o.CertDir + "/tls.crt"
-		keyfile = o.CertDir + "/tls.key"
-	}
 
 	config := &tls.Config{
 		ClientCAs: x509.NewCertPool(),
 	}
 
+	// ca
 	if cafile != "" {
 		capem, err := ioutil.ReadFile(cafile)
 		if err != nil {
@@ -49,12 +42,12 @@ func (o *Options) ToTLSConfig() (*tls.Config, error) {
 		config.ClientCAs.AppendCertsFromPEM(capem)
 	}
 
-	if certfile != "" && keyfile != "" {
-		certificate, err := tls.LoadX509KeyPair(cafile, keyfile)
-		if err != nil {
-			return nil, err
-		}
-		config.Certificates = append(config.Certificates, certificate)
+	// cert
+	certificate, err := tls.LoadX509KeyPair(certfile, keyfile)
+	if err != nil {
+		return nil, err
 	}
+	config.Certificates = append(config.Certificates, certificate)
+
 	return config, nil
 }
