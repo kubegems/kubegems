d8cfa22467c80c5c1a71632a6e2ebfb2648f158e
likun.zhang <likun.zhang@cloudminds.com>
2022-02-22 19:10:19 +0800

    perf(config): update default configuration

--- pkg/utils/argo/options.go
+++ pkg/utils/argo/options.go
@@ -1,5 +1,11 @@
 package argo
 
+import (
+	"fmt"
+
+	"kubegems.io/pkg/apis/gems"
+)
+
 type Options struct {
 	Addr     string `json:"addr" description:"argocd host"`
 	Token    string `json:"token" description:"argocd token,if empty generate from username password"`
@@ -9,10 +15,11 @@ type Options struct {
 
 func NewDefaultArgoOptions() *Options {
 	return &Options{
-		Addr: "http://argocd-server.gemcloud-workflow-system",
+		Addr: fmt.Sprintf("http://argocd-server.%s", gems.NamespaceWorkflow),
 		// 保持为空，则使用 kube port forward
 		// https://argoproj.github.io/argo-cd/operator-manual/security/#authentication
 		// 使用的是第 1 种方式，先 user/password 方式,admin登录web页面,从请求中拿到 admin token
-		Token: "",
+		Token:    "",
+		Username: "admin",
 	}
 }
