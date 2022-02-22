dd835ded9e3deb04388fbc24e701d76c605230b2
likun.zhang <likun.zhang@cloudminds.com>
2022-02-22 15:32:53 +0800

    :fire: remove unused options

--- pkg/msgbus/options/options.go
+++ pkg/msgbus/options/options.go
@@ -1,7 +1,31 @@
 package options
 
-import "kubegems.io/pkg/service/options"
+import (
+	"kubegems.io/pkg/utils/argo"
+	"kubegems.io/pkg/utils/database"
+	"kubegems.io/pkg/utils/oauth"
+	"kubegems.io/pkg/utils/redis"
+	"kubegems.io/pkg/utils/system"
+)
 
-type Options = options.Options
+type Options struct {
+	System   *system.Options   `json:"system,omitempty"`
+	Argo     *argo.Options     `json:"argo,omitempty"`
+	JWT      *oauth.JWTOptions `json:"jwt,omitempty"`
+	LogLevel string            `json:"logLevel,omitempty"`
+	Mysql    *database.Options `json:"mysql,omitempty"`
+	Redis    *redis.Options    `json:"redis,omitempty"`
+}
 
-var DefaultOptions = options.DefaultOptions
+func DefaultOptions() *Options {
+	defaultoptions := &Options{
+		Argo:     argo.NewDefaultArgoOptions(),
+		JWT:      oauth.NewDefaultJWTOptions(),
+		LogLevel: "debug",
+		Mysql:    database.NewDefaultOptions(),
+		Redis:    redis.NewDefaultOptions(),
+		System:   system.NewDefaultOptions(),
+	}
+	defaultoptions.System.Listen = ":8020"
+	return defaultoptions
+}
