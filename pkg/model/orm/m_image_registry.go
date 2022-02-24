266a6398d5c38d2b9ead689e83cc1a7f9e3bc4ac
yu.deng <yu.deng@cloudminds.com>
2022-02-24 16:52:12 +0800

    error fields fix && methods fix

--- pkg/model/orm/m_image_registry.go
+++ pkg/model/orm/m_image_registry.go
@@ -6,15 +6,15 @@ import (
 
 // +gen type:object pkcolume:id pkfield:ID preloads:Project
 type Registry struct {
-	ID              uint   `gorm:"primarykey"`
-	RegistryName    string `gorm:"type:varchar(50);uniqueIndex:uniq_idx_project_registry;"`
-	RegistryAddress string `gorm:"type:varchar(512)"`
-	Username        string `gorm:"type:varchar(50)"`
-	Password        string `gorm:"type:varchar(512)"`
-	Creator         *User
-	UpdateTime      *time.Time
-	CreatorID       uint
-	Project         *Project `gorm:"constraint:OnUpdate:RESTRICT,OnDelete:CASCADE;"`
-	ProjectID       uint     `grom:"uniqueIndex:uniq_idx_project_registry;"`
-	IsDefault       bool
+	ID         uint   `gorm:"primarykey"`
+	Name       string `gorm:"type:varchar(50);uniqueIndex:uniq_idx_project_registry;"`
+	Address    string `gorm:"type:varchar(512)"`
+	Username   string `gorm:"type:varchar(50)"`
+	Password   string `gorm:"type:varchar(512)"`
+	Creator    *User
+	UpdateTime *time.Time
+	CreatorID  uint
+	Project    *Project `gorm:"constraint:OnUpdate:RESTRICT,OnDelete:CASCADE;"`
+	ProjectID  uint     `grom:"uniqueIndex:uniq_idx_project_registry;"`
+	IsDefault  bool
 }
