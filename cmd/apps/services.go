57ae4b9db9ab283c0efd277cd26d8001c98dcc35
likun.zhang <likun.zhang@cloudminds.com>
2022-02-25 17:04:53 +0800

    perf(services): add init deps

--- cmd/apps/services.go
+++ cmd/apps/services.go
@@ -1,18 +1,37 @@
 package apps
 
 import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/spf13/cobra"
+	"kubegems.io/pkg/service/options"
 	"kubegems.io/pkg/services"
+	"kubegems.io/pkg/utils/config"
 )
 
 func NewServicesCmd() *cobra.Command {
+	options := options.DefaultOptions()
+
 	cmd := &cobra.Command{
 		Use:          "services",
 		Short:        "run services",
 		SilenceUsage: true,
-		Run: func(cmd *cobra.Command, _ []string) {
-			services.Run()
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			if err := config.Parse(cmd.Flags()); err != nil {
+				return err
+			}
+			ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+			defer cancel()
+			return services.Run(ctx, options)
 		},
 	}
+	cmd.AddCommand(
+		newGenServiceCfgCmd(),
+		newServiceMigrateCmd(),
+	)
+	config.AutoRegisterFlags(cmd.Flags(), "", options)
 	return cmd
 }
