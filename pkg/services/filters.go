d5b0ebf194c94b0309c72b509296687bde0619e5
yu.deng <dengyu326@gmail.com>
2022-03-01 18:12:02 +0800

    fix dependence

--- pkg/services/filters.go
+++ pkg/services/filters.go
@@ -2,14 +2,15 @@ package services
 
 import (
 	restful "github.com/emicklei/go-restful/v3"
+	"gorm.io/gorm"
 	"kubegems.io/pkg/services/filters"
 	"kubegems.io/pkg/services/options"
 )
 
-func enableFilters(c *restful.Container, opts *options.Options) {
+func enableFilters(c *restful.Container, db *gorm.DB, opts *options.Options) {
 	auth := filters.NewAuthMiddleware(opts.JWT)
 	audit := filters.NewAuditMiddleware()
-	perms := filters.NewPermMiddleware()
+	perms := filters.NewPermMiddleware(db)
 
 	c.Filter(filters.Log)
 	c.Filter(auth.FilterFunc)
