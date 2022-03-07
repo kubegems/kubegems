8498809c060234a3332b2b0b633669c1929686f5
lintong.sun <lintong.sun@cloudminds.com>
2022-03-07 13:45:30 +0800

    🐞 fix(monitor): check rule in use when delete template

--- pkg/models/model_config.go
+++ pkg/models/model_config.go
@@ -11,5 +11,5 @@ type OnlineConfig struct {
 	Name      string         `gorm:"type:varchar(50);primaryKey" binding:"required" json:"name"` // 配置名
 	Content   datatypes.JSON `json:"content"`                                                    // 配置内容
 	CreatedAt *time.Time
-	UpdatedAt *time.Time // 创建时间
+	UpdatedAt *time.Time
 }
