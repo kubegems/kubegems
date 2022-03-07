94c92def856df8dcd150c7a9536319dc3dec722f
yu.deng <dengyu326@gmail.com>
2022-03-07 14:09:13 +0800

    msgbus auth

--- pkg/msgbus/options/options.go
+++ pkg/msgbus/options/options.go
@@ -3,7 +3,7 @@ package options
 import (
 	"kubegems.io/pkg/utils/argo"
 	"kubegems.io/pkg/utils/database"
-	"kubegems.io/pkg/utils/oauth"
+	"kubegems.io/pkg/utils/jwt"
 	"kubegems.io/pkg/utils/redis"
 	"kubegems.io/pkg/utils/system"
 )
@@ -11,7 +11,7 @@ import (
 type Options struct {
 	System   *system.Options   `json:"system,omitempty"`
 	Argo     *argo.Options     `json:"argo,omitempty"`
-	JWT      *oauth.JWTOptions `json:"jwt,omitempty"`
+	JWT      *jwt.Options      `json:"jwt,omitempty"`
 	LogLevel string            `json:"logLevel,omitempty"`
 	Mysql    *database.Options `json:"mysql,omitempty"`
 	Redis    *redis.Options    `json:"redis,omitempty"`
@@ -20,7 +20,7 @@ type Options struct {
 func DefaultOptions() *Options {
 	defaultoptions := &Options{
 		Argo:     argo.NewDefaultArgoOptions(),
-		JWT:      oauth.NewDefaultJWTOptions(),
+		JWT:      jwt.DefaultOptions(),
 		LogLevel: "debug",
 		Mysql:    database.NewDefaultOptions(),
 		Redis:    redis.NewDefaultOptions(),
