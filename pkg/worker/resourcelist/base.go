285ad8c501d45ecd3cbf34248ef292fc54393110
sunlintong <sunlintongshuai@gmail.com>
2022-03-11 16:39:09 +0800

    🦄 refactor(worker): resource list use new client lib

--- pkg/worker/resourcelist/base.go
+++ pkg/worker/resourcelist/base.go
@@ -1,11 +1,6 @@
 package resourcelist
 
 import (
-	"context"
-	"net/url"
-	"strings"
-
-	promemodel "github.com/prometheus/common/model"
 	"github.com/robfig/cron/v3"
 	"kubegems.io/pkg/log"
 	"kubegems.io/pkg/utils/agents"
@@ -36,50 +31,4 @@ func (c *ResourceCache) Start() {
 		log.Error(err, "environment sync")
 	}
 	cron.Start()
-}
-
-type PromeResp struct {
-	Message           string `json:"Message"`
-	promemodel.Vector `json:"Data"`
-	ErrorData         interface{} `json:"ErrorData"`
-}
-
-func (c *ResourceCache) getPrometheusResponseWithCluster(cluster, namespace, promql string) (PromeResp, error) {
-	promql = strings.ReplaceAll(promql, "__namespace__", namespace)
-	values := url.Values{}
-	values.Add("query", promql)
-
-	ctx := context.TODO()
-
-	ret := promemodel.Vector{}
-	cli, err := c.Agents.ClientOf(ctx, cluster)
-	if err != nil {
-		return PromeResp{Vector: ret}, err
-	}
-	if err := cli.DoRequest(ctx, agents.Request{
-		Path:  "/custom/prometheus/v1/vector",
-		Query: values,
-		Into:  agents.WrappedResponse(&ret),
-	}); err != nil {
-		log.Error(err, "exec", "cluster", cluster, "promql", promql)
-	}
-
-	return PromeResp{Vector: ret}, err
-}
-
-// 只返回有环境标签的namespace
-func (c *ResourceCache) collectNamespaces(cluster string) []string {
-	ret := []string{}
-	resp, err := c.getPrometheusResponseWithCluster(cluster, "", `gems_namespace_labels{environment !=""}`)
-	if err != nil {
-		log.Error(err, "get prometheus response")
-		return ret
-	}
-	for _, sample := range resp.Vector {
-		ns, ok := sample.Metric[NamespaceKey]
-		if ok {
-			ret = append(ret, string(ns))
-		}
-	}
-	return ret
 }
