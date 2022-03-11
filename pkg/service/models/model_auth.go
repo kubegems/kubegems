6cde1038b05114961d1f0c6921f8c7d2572f9361
yu.deng <dengyu326@gmail.com>
2022-03-11 09:49:23 +0800

    validate authsource

--- pkg/service/models/model_auth.go
+++ pkg/service/models/model_auth.go
@@ -31,21 +31,21 @@ func (AuthSourceSimple) TableName() string {
 }
 
 type AuthSourceConfig struct {
-	AuthURL     string   `json:"authURL,omitempty"`
-	TokenURL    string   `json:"tokenURL,omitempty"`
-	UserInfoURL string   `json:"userInfoURL,omitempty"`
-	RedirectURL string   `json:"redirectURL,omitempty"`
-	AppID       string   `json:"appID,omitempty"`
-	AppSecret   string   `json:"appSecret,omitempty"`
+	AuthURL     string   `json:"authURL,omitempty" binding:"url,require_with=TokenURL UserInfoURL RedirectURL AppID AppSecret"`
+	TokenURL    string   `json:"tokenURL,omitempty" binding:"url,require_with=AuthURL UserInfoURL RedirectURL AppID AppSecret"`
+	UserInfoURL string   `json:"userInfoURL,omitempty" binding:"url,require_with=AuthURL TokenURL RedirectURL AppID AppSecret"`
+	RedirectURL string   `json:"redirectURL,omitempty" binding:"url,require_with=AuthURL TokenURL UserInfoURL AppID AppSecret"`
+	AppID       string   `json:"appID,omitempty" binding:"require_with=AuthURL TokenURL UserInfoURL RedirectURL AppSecret"`
+	AppSecret   string   `json:"appSecret,omitempty" binding:"require_with=AuthURL TokenURL UserInfoURL RedirectURL AppID"`
 	Scopes      []string `json:"scopes,omitempty"`
 
 	// ldap
 	Name         string `json:"name,omitempty"`
-	LdapAddr     string `json:"ldapaddr,omitempty"`
-	BaseDN       string `json:"basedn,omitempty"`
-	EnableTLS    bool   `json:"enableTLS,omitempty"`
-	BindUsername string `json:"binduser,omitempty"`
-	BindPassword string `json:"password,omitempty"`
+	LdapAddr     string `json:"ldapaddr,omitempty" binding:"require_with=BaseDN EnableTLS BindUsername BindPassword"`
+	BaseDN       string `json:"basedn,omitempty" binding:"require_with=LdapAddr EnableTLS BindUsername BindPassword"`
+	EnableTLS    bool   `json:"enableTLS,omitempty" binding:"require_with=LdapAddr BaseDN BindUsername BindPassword"`
+	BindUsername string `json:"binduser,omitempty" binding:"require_with=LdapAddr BaseDN EnableTLS BindPassword"`
+	BindPassword string `json:"password,omitempty" binding:"require_with=LdapAddr BaseDN EnableTLS BindUsername"`
 }
 
 func (cfg *AuthSourceConfig) Scan(value interface{}) error {
