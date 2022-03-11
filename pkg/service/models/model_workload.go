d5553d9f5fade6d31bb662361d5241c2fea8f817
lintong.sun <lintong.sun@cloudminds.com>
2022-03-11 17:04:52 +0800

    🎈 perf(logoperator): remove unused code

--- pkg/service/models/model_workload.go
+++ pkg/service/models/model_workload.go
@@ -24,7 +24,7 @@ const (
 	resourceMemory = "memory"
 
 	minCPULimitCores    = 0.1      // 10m
-	minMemoryLimitBytes = 100 * Mi // 10Mi
+	minMemoryLimitBytes = 100 * Mi // 100Mi
 )
 
 // Workload workload资源使用清单
