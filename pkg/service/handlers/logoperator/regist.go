8a70c5e57ffd162a8cd20d792c96dc46267538b9
zhanghe9702 <zhanghe9702@163.com>
2022-03-15 09:03:16 +0800

    use name as url resource

--- pkg/service/handlers/logoperator/regist.go
+++ pkg/service/handlers/logoperator/regist.go
@@ -13,7 +13,7 @@ func (h *LogOperatorHandler) RegistRouter(rg *gin.RouterGroup) {
 	// TODO: uncomment graph & nodeagent uri ,it's tested
 	// rg.GET("/logging/cluster/:cluster/namespaces/:namespace/flows/:flowid/graph", h.Graph)
 	// rg.GET("/logging/cluster/:cluster/namespaces/:namespace/flows/:flowid/metircs", h.Metrics)
-	rg.GET("/logging/cluster/:cluster/tenant/:tenant_id/flows", h.CheckByTenantID, h.Flows)
-	rg.GET("/logging/cluster/:cluster/tenant/:tenant_id/outputs", h.CheckByTenantID, h.Outputs)
+	rg.GET("/logging/cluster/:cluster/tenant/:tenant_id/flows", h.Flows)
+	rg.GET("/logging/cluster/:cluster/tenant/:tenant_id/outputs", h.Outputs)
 	// rg.POST("/logging/cluster/:cluster/namespaces/:namespace/nodeagent/:name", h.CreateNodeAgentLogCollector)
 }
