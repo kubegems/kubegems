0064fd65a35cbac4bae253e4ce464c872f420800
sunlintong <sunlintongshuai@gmail.com>
2022-04-27 15:34:40 +0800

    📃 docs: make swagger

--- pkg/installer/controllers/plugin_controller.go
+++ pkg/installer/controllers/plugin_controller.go
@@ -59,7 +59,7 @@ func NewAndSetupPluginReconciler(ctx context.Context, mgr manager.Manager, optio
 // move the current state of the cluster closer to the desired state.
 //
 // For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.10.0/pkg/reconcile
+// -  https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.10.0/pkg/reconcile
 func (r *PluginReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	log := logr.FromContextOrDiscard(ctx)
 
