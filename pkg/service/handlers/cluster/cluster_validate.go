0d710ee067eb0780811eeca9f044e52276504f58
sunlintong <sunlintongshuai@gmail.com>
2022-05-24 15:44:33 +0800

    🐞 fix(cluster): update validate check plugin when add cluster

--- pkg/service/handlers/cluster/cluster_validate.go
+++ pkg/service/handlers/cluster/cluster_validate.go
@@ -1,11 +1,14 @@
 package clusterhandler
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/gin-gonic/gin"
+	"github.com/pkg/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	kversion "k8s.io/apimachinery/pkg/version"
+	pluginsv1beta1 "kubegems.io/pkg/apis/plugins/v1beta1"
 	"kubegems.io/pkg/log"
 	"kubegems.io/pkg/service/handlers"
 	"kubegems.io/pkg/utils/kube"
@@ -26,6 +29,8 @@ type ValidateKubeConfigResp struct {
 	Connectable bool   `json:"connectable,omitempty"`
 	Message     string `json:"message,omitempty"`
 }
+
+const localPluginURL = "/apis/plugins.kubegems.io/v1beta1/namespaces/kubegems-local/plugins/kubegems-local-stack"
 
 // ValidateKubeConfig 添加cluster前的kubeconfig检测接口，验证kubeconfig，返回集群信息和可用的storageClass列表
 // @Tags         Cluster
@@ -73,6 +78,25 @@ func (h *ClusterHandler) ValidateKubeConfig(c *gin.Context) {
 	}
 	for _, sc := range scList.Items {
 		resp.StorageClasses = append(resp.StorageClasses, sc.GetName())
+	}
+
+	bts, err := clientSet.Discovery().RESTClient().Get().AbsPath(localPluginURL).Do(ctx).Raw()
+	if err == nil {
+		plugin := pluginsv1beta1.Plugin{}
+		if err := json.Unmarshal(bts, &plugin); err != nil {
+			handlers.NotOK(c, errors.Wrap(err, "unmarshal plugin"))
+			return
+		}
+		values := map[string]interface{}{}
+		json.Unmarshal(plugin.Spec.Values.Raw, &values)
+		if global, ok := values["global"]; ok {
+			if globalMap, ok := global.(map[string]interface{}); ok {
+				if name, ok := globalMap["clusterName"]; ok {
+					resp.ClusterName = name.(string)
+				}
+			}
+		}
+		resp.ExistInstaller = true
 	}
 	handlers.OK(c, resp)
 }
