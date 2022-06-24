cf9688d25c3e7b5376f1b725c064c831f413c953
yu.deng <dengyu326@gmail.com>
2022-06-24 10:52:00 +0800

    plugins public

--- pkg/service/routers/plugin.go
+++ pkg/service/routers/plugin.go
@@ -1,13 +1,88 @@
-//go:build !internal
-// +build !internal
-
 package routers
 
 import (
+	"context"
+	"net/http"
+	"strings"
+
 	"github.com/gin-gonic/gin"
+	cserice "kubegems.io/configer/service"
 	"kubegems.io/kubegems/pkg/service/handlers/base"
+	"kubegems.io/kubegems/pkg/service/models"
+	"kubegems.io/kubegems/pkg/utils/agents"
 )
 
 func registPlugins(rg *gin.RouterGroup, basehandler base.BaseHandler) error {
+	configPlugin, err := cserice.NewPlugin(&PluginInfoGetter{
+		BaseHandler: basehandler,
+	}, basehandler.GetDB())
+	if err != nil {
+		return err
+	}
+	if err := configPlugin.InitDatabase(); err != nil {
+		return err
+	}
+	configPlugin.Handler.RegistRouter(rg)
 	return nil
+}
+
+type PluginInfoGetter struct {
+	base.BaseHandler
+}
+
+func (p *PluginInfoGetter) ClusterNameOf(tenant, project, environment string) (clusterName string) {
+	cluster := &models.Cluster{}
+	sql := `select clusters.cluster_name as cluster_name, clusters.id as id from clusters
+	left join environments ON environments.cluster_id = clusters.id
+	left join projects on projects.id = environments.project_id
+	left join tenants on tenants.id = projects.tenant_id
+	where environment_name = @environment and project_name = @project and tenant_name = @tenant limit 1`
+	if err := p.GetDB().Raw(sql, map[string]interface{}{"tenant": tenant, "project": project, "environment": environment}).Find(&cluster).Error; err != nil {
+		return ""
+	}
+	return cluster.ClusterName
+}
+
+func (p *PluginInfoGetter) NacosInfoOf(clusterName string) (addr, username, password string, err error) {
+	cli, err := p.GetAgents().ClientOf(context.Background(), clusterName)
+	if err != nil {
+		return "", "", "", err
+	}
+	u := cli.BaseAddr()
+	h := u.String() + "/v1/service-proxy"
+	return h, "nacos", "nacos", nil
+}
+
+func (p *PluginInfoGetter) RoundTripperOf(clusterName string) (rt http.RoundTripper) {
+	cli, _ := p.GetAgents().ClientOf(context.Background(), clusterName)
+	return RoundTripOf(cli)
+}
+
+func (p *PluginInfoGetter) Username(c *gin.Context) string {
+	u, exist := p.ContextUserOperator.GetContextUser(c)
+	if !exist {
+		return ""
+	}
+	return u.GetUsername()
+}
+
+// TODO: merge with utils
+
+func RoundTripOf(cli agents.Client) http.RoundTripper {
+	return RoundTripperFunc(func(req *http.Request) (*http.Response, error) {
+		realPath := strings.TrimPrefix(req.URL.Path, cli.BaseAddr().Path)
+		return cli.DoRawRequest(req.Context(), agents.Request{
+			Method:  req.Method,
+			Path:    realPath,
+			Query:   req.URL.Query(),
+			Headers: req.Header,
+			Body:    req.Body,
+		})
+	})
+}
+
+type RoundTripperFunc func(req *http.Request) (*http.Response, error)
+
+func (c RoundTripperFunc) RoundTrip(req *http.Request) (*http.Response, error) {
+	return c(req)
 }
