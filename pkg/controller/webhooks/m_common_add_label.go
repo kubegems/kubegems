ae8a051bf9cbc37440c87b213912ea29eb5ad46f
sunlintong <sunlintongshuai@gmail.com>
2022-06-27 10:48:50 +0800

    🐞 fix(controller): fix flow webhook

--- pkg/controller/webhooks/m_common_add_label.go
+++ pkg/controller/webhooks/m_common_add_label.go
@@ -21,74 +21,81 @@ import (
 )
 
 func (r *LabelInjectorMutate) CommonInjectLabel(ctx context.Context, req admission.Request) admission.Response {
-	var obj runtime.Object
-	switch req.Kind.Kind {
-	case "Pod":
-		obj = &corev1.Pod{}
-	case "ConfigMap":
-		obj = &corev1.ConfigMap{}
-	case "Secret":
-		obj = &corev1.Secret{}
-	case "Service":
-		obj = &corev1.Service{}
-	case "Job":
-		obj = &batchv1.Job{}
-	case "CronJob":
-		obj = &batchv1beta1.CronJob{}
-	case "Deployment":
-		obj = &appsv1.Deployment{}
-	case "StatefulSet":
-		obj = &appsv1.StatefulSet{}
-	case "DaemonSet":
-		obj = &appsv1.DaemonSet{}
-	case "PersistentVolumeClaim":
-		obj = &corev1.PersistentVolumeClaim{}
-	case "Flow":
-		obj = &loggingv1beta1.Flow{}
-	case "Output":
-		obj = &loggingv1beta1.Output{}
-	default:
-		r.Log.Error(errors.New("not support"), req.Kind.String())
-		return admission.Allowed("pass")
-	}
-
-	if err := r.decoder.Decode(req, obj); err != nil {
-		r.Log.Error(err, "failed ")
-		return admission.Allowed("pass")
-	}
-
-	metadata, err := meta.Accessor(obj)
-	if err != nil {
-		r.Log.Error(err, "getmeta")
-		return admission.Allowed("pass")
-	}
-	originlabels := metadata.GetLabels()
-	if originlabels == nil {
-		originlabels = map[string]string{}
-	}
-
-	ns, err := r.getAndMutateNS(ctx, req)
-	if err != nil {
-		r.Log.Error(err, "getAndMutateNSLabel")
-		return admission.Allowed("pass")
-	}
-
-	newLabels := r.getComonLabels(ctx, ns)
-	for k, v := range newLabels {
-		originlabels[k] = v
-	}
-
-	metadata.SetLabels(originlabels)
 	switch req.Operation {
 	case v1.Create, v1.Update:
+		var obj runtime.Object
+		switch req.Kind.Kind {
+		case "Pod":
+			obj = &corev1.Pod{}
+		case "ConfigMap":
+			obj = &corev1.ConfigMap{}
+		case "Secret":
+			obj = &corev1.Secret{}
+		case "Service":
+			obj = &corev1.Service{}
+		case "Job":
+			obj = &batchv1.Job{}
+		case "CronJob":
+			obj = &batchv1beta1.CronJob{}
+		case "Deployment":
+			obj = &appsv1.Deployment{}
+		case "StatefulSet":
+			obj = &appsv1.StatefulSet{}
+		case "DaemonSet":
+			obj = &appsv1.DaemonSet{}
+		case "PersistentVolumeClaim":
+			obj = &corev1.PersistentVolumeClaim{}
+		case "Flow":
+			obj = &loggingv1beta1.Flow{}
+		case "Output":
+			obj = &loggingv1beta1.Output{}
+		default:
+			r.Log.Error(errors.New("not support"), req.Kind.String())
+			return admission.Allowed("pass")
+		}
+
+		if err := r.decoder.Decode(req, obj); err != nil {
+			r.Log.Error(err, "failed ")
+			return admission.Allowed("pass")
+		}
+
+		metadata, err := meta.Accessor(obj)
+		if err != nil {
+			r.Log.Error(err, "getmeta")
+			return admission.Allowed("pass")
+		}
+		originlabels := metadata.GetLabels()
+		if originlabels == nil {
+			originlabels = map[string]string{}
+		}
+
+		ns, err := r.getAndMutateNS(ctx, req)
+		if err != nil {
+			r.Log.Error(err, "getAndMutateNSLabel")
+			return admission.Allowed("pass")
+		}
+
+		newLabels := r.getComonLabels(ctx, ns)
+		for k, v := range newLabels {
+			originlabels[k] = v
+		}
+
+		metadata.SetLabels(originlabels)
 		modifyed, e := json.Marshal(metadata)
 		if e != nil {
 			r.Log.WithName(req.Name).WithName(req.Namespace).Error(e, "error")
 		}
 		return admission.PatchResponseFromRaw(req.Object.Raw, modifyed)
+	case v1.Delete:
+		_, err := r.getAndMutateNS(ctx, req)
+		if err != nil {
+			r.Log.Error(err, "getAndMutateNSLabel")
+			return admission.Allowed("pass")
+		}
 	default:
 		return admission.Allowed("pass")
 	}
+	return admission.Allowed("pass")
 }
 
 func (r *LabelInjectorMutate) getComonLabels(ctx context.Context, ns *corev1.Namespace) map[string]string {
