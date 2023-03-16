98eafaca2e6125462ff81179c0166ec9eb91af3a
cnfatal <cnfatal@gmail.com>
2023-03-15 22:10:02 -0600

    fix(plugin): deepcopy map use native

--- pkg/apis/plugins/v1beta1/types_values.go
+++ pkg/apis/plugins/v1beta1/types_values.go
@@ -16,19 +16,13 @@ package v1beta1
 
 import (
 	"bytes"
-	"encoding/gob"
 	"encoding/json"
 	"errors"
 )
 
 type Values struct {
-	Raw    []byte                 `json:"-"`
-	Object map[string]interface{} `json:"-"`
-}
-
-func init() {
-	// https://pkg.go.dev/encoding/gob#Register
-	gob.Register(map[string]interface{}{})
+	Raw    []byte         `json:"-"`
+	Object map[string]any `json:"-"`
 }
 
 // DeepCopy indicate how to do a deep copy of Values type
@@ -36,17 +30,30 @@ func (v *Values) DeepCopy() *Values {
 	if v == nil {
 		return nil
 	}
-	out := Values{}
-	if v.Raw != nil {
-		out.Raw = make([]byte, len(v.Raw))
-		copy(out.Raw, v.Raw)
+	return &Values{
+		Raw: bytes.Clone(v.Raw),
+		// nolint: forcetypeassert
+		Object: deepCopyAny(v.Object).(map[string]any),
 	}
-	if v.Object != nil {
-		buf := new(bytes.Buffer)
-		gob.NewEncoder(buf).Encode(v.Object)
-		gob.NewDecoder(buf).Decode(&out.Object)
+}
+
+func deepCopyAny(in any) any {
+	switch val := in.(type) {
+	case map[string]any:
+		out := make(map[string]any, len(val))
+		for k, v := range val {
+			out[k] = deepCopyAny(v)
+		}
+		return out
+	case []any:
+		out := make([]any, len(val))
+		for i, v := range val {
+			out[i] = deepCopyAny(v)
+		}
+		return out
+	default:
+		return val
 	}
-	return &out
 }
 
 func (v Values) FullFill() Values {
@@ -69,7 +76,7 @@ func (v *Values) UnmarshalJSON(in []byte) error {
 	}
 	v.Raw = make([]byte, len(in))
 	copy(v.Raw, in)
-	val := map[string]interface{}(nil)
+	val := map[string]any(nil)
 	if err := json.Unmarshal(in, &val); err != nil {
 		return err
 	}
@@ -91,10 +98,10 @@ func (re Values) MarshalJSON() ([]byte, error) {
 
 // https://github.com/helm/helm/blob/bed1a42a398b30a63a279d68cc7319ceb4618ec3/pkg/chartutil/coalesce.go#L37
 // helm CoalesceValues cant handle nested null,like `{a: {b: null}}`, which want to be `{}`
-func RemoveNulls(m interface{}) {
-	if m, ok := m.(map[string]interface{}); ok {
+func RemoveNulls(m any) {
+	if m, ok := m.(map[string]any); ok {
 		for k, v := range m {
-			if val, ok := v.(map[string]interface{}); ok {
+			if val, ok := v.(map[string]any); ok {
 				RemoveNulls(val)
 				if len(val) == 0 {
 					delete(m, k)
